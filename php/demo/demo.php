cf6de78d2522ff77093cf718fb81dff76b98a2f7
wangyu <swunwy@163.com>
2017-11-30 22:54:34 +0800

    practice&test

---
+++ php/demo/demo.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+$str = "Once upon a time there was a turtle";
+//将字符串分解为独立的字
+$words = explode(' ',$str);
+//反转这个字数组
+$words = array_reverse($words);
+//重建反转后的字符串
+$str = implode(' ',$words);
+print_r($str);
